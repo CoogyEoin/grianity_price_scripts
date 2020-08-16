1339767d8162ea73ced459a2df10affdc3095172
Eoin Coogan <38385465+CoogyEoin@users.noreply.github.com>
2020-08-16 18:28:37 +0100

    Update caiso_lambda_script.py

--- caiso_lambda_script.py
+++ caiso_lambda_script.py
@@ -20,56 +20,46 @@ def get_timestamp():
     
     
 def get_value():
-    URL = 'https://www.iso-ne.com/'
+    URL = 'http://oasis.caiso.com/oasisapi/prc_hub_lmp/PRC_HUB_LMP.html'
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    td_element = soup.find('td', class_ ='homepage-price')
-    lpm_text = td_element.text
-    return lpm_text[1:]
-    
+    lmp_values=[]
+    lmp_dict = {}
+    for i in soup.find_all('tr', class_='datarow'):
+        for j in i.findChildren('td'):
+            #Removes the rediculous whitespace in CAISO webpage
+            breadcrum = "".join(line.strip() for line in j.text.split("\n"))
+            lmp_values.append(breadcrum)
 
-def lambda_handler(event, context):
-    """
+    lmp_value = lmp_values[1]
+    
+    #Returns lmp_value without the first character ( $ )
+    return lmp_value[1:]
+    
+    
+def put_item(lmp_value, time_stamp, region):
     client = boto3.client('dynamodb', region_name=os.environ['aws_region'])
-    
-    time_stamp = get_timestamp()
-    url = 'https://hourlypricing.comed.com/api?type=5minutefeed&datestart=' + time_stamp +'&dateend=' + time_stamp;
-    print('URL: ' + url)
-    response = requests.get(url)
-	
-    json_response = response.json()[0]
-    price = json_response['price']
-    
-    dynamodbresponse = client.put_item(
+    return client.put_item(
             TableName=os.environ['table_name'],
             Item={
-                'price': { "S": price},
+                'price': { "S": lmp_value},
+                'region_name': { "S": region},
                 'timestamp': { "S": time_stamp}
             }
             )
-    return dynamodbresponse
-    """
+    
+
+def lambda_handler(event, context):
+
     client = boto3.client('dynamodb', region_name=os.environ['aws_region'])
-    region = "iso-ne"
     
     try:
-        URL = 'http://oasis.caiso.com/oasisapi/prc_hub_lmp/PRC_HUB_LMP.html'
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        #soup = soup.prettify() 
-
-        lpm_values=[]
-        lpm_dict = {}
-        for i in soup.find_all('tr', class_='datarow'):
-            for j in i.findChildren('td'):
-                #Removes the rediculous whitespace in CAISO webpage
-                breadcrum = "".join(line.strip() for line in j.text.split("\n"))
-                lpm_values.append(breadcrum)
-
-        print(lpm_values[1])
-
+        lmp_value = get_value()
+        time_stamp = get_timestamp()
+        response = put_item(lmp_value, time_stamp, "caiso")
+        
+        return response
 
     except Exception as e:
         raise e
-
